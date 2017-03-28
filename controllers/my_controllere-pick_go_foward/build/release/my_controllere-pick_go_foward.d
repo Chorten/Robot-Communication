9cfc93b43d2a60879885da4a4b838873d4c5a9dd
Asheq Reza <Asheq Reza>
2017-03-28 13:50:31 -0400

    added the webot world with two temporary Nao robots in a soccer stadium field

---
+++ controllers/my_controllere-pick_go_foward/build/release/my_controllere-pick_go_foward.d
@@ -0,0 +1,5 @@
+build/release/my_controllere-pick_go_foward.o: \
+ my_controllere-pick_go_foward.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/differential_wheels.h \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h
